5b7f10f13264a08bfa4d34e18a2ee9ebbf56354d
olebroetje <olebroetje@gmail.com>
2022-02-11 23:48:32 +0100

    updated lines_image.py and cleaned up unused function in lines.py

--- lines.py
+++ lines.py
@@ -1,22 +1,8 @@
 import cv2
-import numpy as np
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
-
-
-def draw_lines(img, _lines, color=None, thickness=1):
-    if color is None:
-        color = [0, 0, 255]
-    if _lines is None:
-        return
-    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-    for line in _lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(line_img, (x1, y1), (x2, y2), color, thickness)
-    img_return = cv2.addWeighted(img, 0.8, line_img, 1.0, 0.0)
-    return img_return
 
 
 while True:
