f5f6b9d2378132a332f16c651dc01f9ddfd72cff
olebroetje <71212155+olebroetje@users.noreply.github.com>
2022-03-02 23:18:40 +0100

    Add files via upload

---
+++ sketch_jan15b.ino
@@ -0,0 +1,40 @@
+#include <Adafruit_MotorShield.h>
+#define SHOULD_DRIVE 1
+#define MOTORCONTROL_RIGHT 2
+#define MOTORCONTROL_LEFT 3
+#define MOTORCONTOL_STRAIGHT 4
+#define INPUT_CONTROL 6
+#define OUTPUT_CONTROL 7
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *right = AFMS.getMotor(2);
+Adafruit_DCMotor *left = AFMS.getMotor(1);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LINE_RIGHT, INPUT);
+  pinMode(LINE_LEFT, INPUT);
+  pinMode(INPUT_CONTROL, INPUT);
+  pinMode(SHOULD_DRIVE, INPUT);
+  pinMode(OUTPUT_CONTROL, OUTPUT);
+  AFMS.begin(1900);
+  right->setSpeed(150);
+  left->setSpeed(150);
+}
+
+void loop() {
+  while (digitalRead(SHOULD_DRIVE) == true) {
+    if (digitalRead(MOTORCONTOL_STRAIGHT) == HIGH) {
+      right->run(FORWARD)
+      left->run(FORWARD)
+    } else if (digitalRead(MOTORCONTROL_RIGHT == HIGH)) {
+      right->run(BACKWARD)
+      left->run(FORWARD)
+    } else if (digitalRead(MOTORCONTROL_LEFT == HIGH) {
+    right->run(FORWARD)
+      left->run(BACKWARD)
+    }
+  }
+}
